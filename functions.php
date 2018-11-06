0e11e533a6d70c6eaeaa9911aabd39c6c83d3981
zampiello alexia <alexzamp@hotmail.fr>
2018-11-06 14:40:22 +0100

    style desactiver

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function themealex_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'themealex_add_woocommerce_support' );
+
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+
+
+
+
+ ?>
